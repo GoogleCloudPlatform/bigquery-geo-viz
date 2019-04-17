5289e7470df4ef9f995464abefc3b56cbb9d37b9
Don McCurdy <donmccurdy@google.com>
2019-04-17 11:09:18 -0700

    Use deck.gl v7.0.0-beta.3

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -16,8 +16,8 @@
 
 import { ChangeDetectionStrategy, Component, ElementRef, Input, ViewChild, AfterViewInit } from '@angular/core';
 import { StylesService, StyleRule } from '../services/styles.service';
-import { Deck } from '@deck.gl/core';
 import { GeoJsonLayer } from '@deck.gl/layers';
+import { GoogleMapsOverlay } from '@deck.gl/google-maps';
 import bbox from '@turf/bbox';
 import { GeoJSONService, GeoJSONFeature } from '../services/geojson.service';
 
@@ -54,10 +54,7 @@ export class MapComponent implements AfterViewInit {
   private _styles: StyleRule[] = [];
   private _geoColumn: string;
 
-  private _isMounted = false;
-  private _canvasEl: HTMLCanvasElement = null;
-  private _deckInstance: Deck = null;
-  private _overlay: google.maps.OverlayView = null;
+  private _deckLayer: GoogleMapsOverlay = null;
   private _hoveredFeature: GeoJSONFeature = null;
 
   @Input()
@@ -100,54 +97,16 @@ export class MapComponent implements AfterViewInit {
         this.map.data.addListener('click', (e) => {
           this.showInfoWindow(e.feature, e.latLng);
         });
-        this._overlay = new google.maps.OverlayView();
-        this._overlay.draw = () => this._draw();
-        this._overlay.setMap(this.map);
+        this._deckLayer = new GoogleMapsOverlay({layers: []});
+        this._deckLayer.setMap(this.map);
         this.map.addListener('click', (e) => this._onClick(e));
         this.map.addListener('mousemove', (e) => this._onMousemove(e));
       });
-
-    // Create DeckGL instance.
-    this._canvasEl = document.createElement('canvas');
-    this._canvasEl.width = this.mapEl.nativeElement.clientWidth;
-    this._canvasEl.height = this.mapEl.nativeElement.clientHeight;
-    this._canvasEl.style.position = 'absolute'; // needed?
-
-    this._deckInstance = new Deck({
-      canvas: this._canvasEl,
-      width: this._canvasEl.width,
-      height: this._canvasEl.height,
-      initialViewState: INITIAL_VIEW_STATE,
-      // Google Maps Platform has no rotating capabilities, so we disable rotation here.
-      controller: {
-        scrollZoom: false,
-        dragPan: false,
-        dragRotate: false,
-        doubleClickZoom: false,
-        touchZoom: false,
-        touchRotate: false,
-        keyboard: false,
-      },
-      layers: []
-    });
-
-    window.addEventListener('resize', () => this._onResize());
-  }
-
-  _onResize() {
-    const width = this.mapEl.nativeElement.clientWidth;
-    const height = this.mapEl.nativeElement.clientHeight;
-    this._canvasEl.width = width;
-    this._canvasEl.height = height;
-
-    if (this._deckInstance) {
-      this._deckInstance.setProps({width, height});
-    }
   }
 
   _onClick(e: google.maps.MouseEvent) {
     const { x, y } = e['pixel'];
-    const picked = this._deckInstance.pickObject({ x, y, radius: 4 });
+    const picked = this._deckLayer.pickObject({ x, y, radius: 4 });
 
     if (picked) {
       this.showInfoWindow(picked.object, e.latLng);
@@ -155,12 +114,8 @@ export class MapComponent implements AfterViewInit {
   }
 
   _onMousemove(e: google.maps.MouseEvent) {
-    if (!this._deckInstance.layerManager) {
-      return;
-    }
-
     const { x, y } = e['pixel'];
-    const picked = this._deckInstance.pickObject({ x, y, radius: 0 });
+    const picked = this._deckLayer.pickObject({ x, y, radius: 0 });
 
     if (picked && this._hoveredFeature !== picked.object) {
       this._hoveredFeature = picked.object;
@@ -173,6 +128,8 @@ export class MapComponent implements AfterViewInit {
    * Converts row objects into GeoJSON, then loads into Maps API.
    */
   updateFeatures() {
+    if (!this.map) return;
+
     this._features = GeoJSONService.rowsToGeoJSON(this._rows, this._geoColumn);
 
     // Fit viewport bounds to the data.
@@ -188,11 +145,11 @@ export class MapComponent implements AfterViewInit {
    * Updates styles applied to all GeoJSON features.
    */
   updateStyles() {
-    if (!this.map) { return; }
+    if (!this.map) return;
     this.styler.uncache();
 
     // Remove old features.
-    this._deckInstance.setProps({ layers: [] });
+    this._deckLayer.setProps({ layers: [] });
 
     // Create GeoJSON layer.
     const colorRe = /(\d+), (\d+), (\d+)/;
@@ -222,7 +179,7 @@ export class MapComponent implements AfterViewInit {
       getRadius: (d) => this.getStyle(d, this._styles, 'circleRadius'),
     });
 
-    this._deckInstance.setProps({ layers: [layer] });
+    this._deckLayer.setProps({ layers: [layer] });
   }
 
   /**
@@ -255,43 +212,5 @@ export class MapComponent implements AfterViewInit {
     this.infoWindow.setContent(`<pre>${JSON.stringify(feature.properties, null, 2)}</pre>`);
     this.infoWindow.open(this.map);
     this.infoWindow.setPosition(latLng);
-  }
-
-  _draw () {
-    // Methods like map.getCenter() and map.getZoom() return rounded values that
-    // don't stay in sync during zoom and pan gestures, so compute center and
-    // zoom from the overlay projection, instead.
-    // Don't call overlay.getPanes() until map has initialized.
-    if (!this._isMounted) {
-      const overlayLayerEl = this._overlay.getPanes().overlayLayer;
-      overlayLayerEl.appendChild(this._canvasEl);
-      this._isMounted = true;
-    }
-
-    const { clientWidth, clientHeight } = this.mapEl.nativeElement;
-    const projection = this._overlay.getProjection();
-
-    // Fit canvas to current viewport.
-    const bounds = this.map.getBounds();
-    const nwContainerPx = new google.maps.Point(0, 0);
-    const nw = projection.fromContainerPixelToLatLng(nwContainerPx);
-    const nwDivPx = projection.fromLatLngToDivPixel(nw);
-    this._canvasEl.style.top = nwDivPx.y + 'px';
-    this._canvasEl.style.left = nwDivPx.x + 'px';
-
-    // Compute fractional zoom.
-    const zoom = Math.log2(projection.getWorldWidth() / TILE_SIZE) - 1;
-
-    // Compute fractional center.
-    const centerPx = new google.maps.Point(clientWidth / 2, clientHeight / 2);
-    const centerContainer = projection.fromContainerPixelToLatLng(centerPx);
-    const latitude = centerContainer.lat();
-    const longitude = centerContainer.lng();
-
-    const deck = this._deckInstance;
-    deck.setProps({ viewState: { zoom, latitude, longitude } });
-    if (deck.layerManager) {
-      deck.animationLoop.redraw();
-    }
   }
 }
